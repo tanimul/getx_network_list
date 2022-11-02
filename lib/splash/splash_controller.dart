d980fb710a13fcc5265ac257be022f2853aab7eb
tanimul <tanimulislam.fuad@gmail.com>
2022-11-02 11:31:43 +0600

    Count Down added using Splash Controller.

--- lib/splash/splash_controller.dart
+++ lib/splash/splash_controller.dart
@@ -2,8 +2,8 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_state_manager/get_state_manager.dart';
-import 'package:getx_network_list/player_list.dart';
+import 'package:getx_network_list/user_list/user_view.dart';
+
 
 class SplashController extends GetxController {
   @override
@@ -16,7 +16,7 @@ class SplashController extends GetxController {
     debugPrint("setTimer");
     Timer(
       const Duration(milliseconds: 3000),
-      () => Get.offAll(const PlayerList()),
+      () => Get.offAll(const UserView()),
     );
   }
 }
