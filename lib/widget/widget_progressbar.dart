e43bf7e7ef8683a63ae352b3aafdd19727dda1a3
tanimul <tanimulislam.fuad@gmail.com>
2022-11-09 15:43:19 +0600

    Progressbar added.

---
+++ lib/widget/widget_progressbar.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CustomProgressIndicator extends StatelessWidget {
+  const CustomProgressIndicator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: SizedBox(
+        height: 50,
+        width: 50,
+        child: CircularProgressIndicator(
+          strokeWidth: 6,
+        ),
+      ),
+    );
+  }
+}
