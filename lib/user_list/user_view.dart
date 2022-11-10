76596036beb3e5aad3cb4b95a9db3ce6adc65acf
tanimul <tanimulislam.fuad@gmail.com>
2022-11-10 10:34:26 +0600

    data retried and Show in list.

--- lib/user_list/user_view.dart
+++ lib/user_list/user_view.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_network_list/model/user_model.dart';
+import 'package:getx_network_list/user_list/user_controller.dart';
+import 'package:getx_network_list/widget/user_item_widget.dart';
+import 'package:getx_network_list/widget/widget_progressbar.dart';
 
 class UserView extends StatelessWidget {
-  const UserView({Key? key}) : super(key: key);
+  final controller = Get.put(UserController());
+
+  UserView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -12,6 +19,28 @@ class UserView extends StatelessWidget {
           style: TextStyle(color: Colors.white),
         ),
         iconTheme: const IconThemeData(color: Colors.white),
+      ),
+      body: RefreshIndicator(
+        onRefresh: () async {
+          controller.getUserList();
+        },
+        child: Center(
+          child: Obx(() {
+            if (controller.isLoading.isFalse) {
+              debugPrint("length: ${controller.userData.length}");
+              return ListView.builder(
+                  itemCount: controller.userData.length,
+                  itemBuilder: (context, index) {
+                    final Users users=controller.userData[index];
+
+                    return(UserItemWidget(users: users));
+
+                  });
+            } else {
+              return const CustomProgressIndicator();
+            }
+          }),
+        ),
       ),
     );
   }
